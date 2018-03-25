cd2efcf20b30cc6b25002c18e90a3bd264ea2f5b
Oleksandra Nadtochii <alexandra.nadtochiy.v@gmail.com>
2018-03-25 23:05:09 +0300

    First test

--- mysite/shortener/tests.py
+++ mysite/shortener/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import unittest
+from django.test import TestCase, Client
 
-# Create your tests here.
+class SimpleTest(unittest.TestCase):
+    def test_details(self):
+        client = Client()
+        response = client.get('/short/')
+        self.assertEqual(response.status_code, 200)
